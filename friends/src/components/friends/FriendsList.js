4dd5378cbd6095be9592f646942f1e5070fd2a72
Candace Wilson <candaceyw@gmail.com>
2020-03-15 23:10:37 -0400

    friends rendering to screen

--- friends/src/components/friends/FriendsList.js
+++ friends/src/components/friends/FriendsList.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {axiosWithAuth} from '../../utils/axiosWithAuth'
 
-const FriendsList = () => {
+const FriendsList = (props) => {
+    const [updated, setUpdate] = useState(false)
+    const [friend, setFriend] = useState({})
+    const [friends, setFriends] = useState([])
+
+    
+    useEffect(() => {
+        axiosWithAuth()
+          .get('/friends')
+          .then(res => 
+            // console.log(res.data)
+            setFriends(res.data)
+            )
+          .catch(err => console.log(err.response));
+        },[updated])
+
+
     return (
         <div>
             <h1>Friends List will be here</h1>
-        </div>
+            <div className="friends">
+            {friends.map(friend => (
+                <div key={friend.id}>
+                <h1>{friend.name}</h1>
+                <p>{friend.age}</p>
+                <p>{friend.email}</p>
+                </div>
+            )
+                
+                )}
+        </div>        </div>
+
     )
 }
 
